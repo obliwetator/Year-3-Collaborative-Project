b7a3561068598415e625001f592d3d13ca117a94
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-03 14:34:11 +0000

    first commit

---
+++ GroupProject/Classes/clsDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql;
+using MySql.Data.MySqlClient;
+
+namespace practical_16_11.Classes
+{
+
+  class clsDatabase
+  {
+    private const string connString = "Server=plesk.remote.ac;database=WS295203_COllabDB;user=WS295203_CollabUser;password=CollabUser1;CharSet=utf8;";
+
+    public MySqlConnection GetConnection()
+    {
+      MySqlConnection cnn = new MySqlConnection(connString);
+
+      return cnn;
+    }
+  }
+}
