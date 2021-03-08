d22f9f223645486b39287944a07e80c3c63768cd
griff <griff@LAPTOP-E5H0KDMB>
2021-03-08 14:34:14 +0000

    Accidentally put the design for the dashbord on the updating page, so I fixed it.

--- GroupProject/AdminDashbord.Designer.cs
+++ GroupProject/AdminDashbord.Designer.cs
@@ -29,32 +29,101 @@ namespace GroupProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnOne = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSetApr = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblDashbord = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnOne
+            // btnDelete
             // 
-            this.btnOne.Location = new System.Drawing.Point(525, 196);
-            this.btnOne.Name = "btnOne";
-            this.btnOne.Size = new System.Drawing.Size(75, 23);
-            this.btnOne.TabIndex = 0;
-            this.btnOne.Text = "button1";
-            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnDelete.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(585, 200);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(170, 70);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Click here to delete a car";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(405, 200);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(170, 70);
+            this.btnInsert.TabIndex = 10;
+            this.btnInsert.Text = "Click here to add a car to the system";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(225, 200);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(170, 70);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Click here to update database";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnSetApr
+            // 
+            this.btnSetApr.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetApr.Location = new System.Drawing.Point(45, 200);
+            this.btnSetApr.Name = "btnSetApr";
+            this.btnSetApr.Size = new System.Drawing.Size(170, 70);
+            this.btnSetApr.TabIndex = 8;
+            this.btnSetApr.Text = "Click here to set APR%";
+            this.btnSetApr.UseVisualStyleBackColor = true;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblUsername.Location = new System.Drawing.Point(40, 80);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(62, 23);
+            this.lblUsername.TabIndex = 7;
+            this.lblUsername.Text = "label1";
+            // 
+            // lblDashbord
+            // 
+            this.lblDashbord.AutoSize = true;
+            this.lblDashbord.Font = new System.Drawing.Font("Arial", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDashbord.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblDashbord.Location = new System.Drawing.Point(200, 0);
+            this.lblDashbord.Name = "lblDashbord";
+            this.lblDashbord.Size = new System.Drawing.Size(398, 57);
+            this.lblDashbord.TabIndex = 6;
+            this.lblDashbord.Text = "Admin Dashbord";
             // 
             // AdminDashbord
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSetApr);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblDashbord);
             this.Name = "AdminDashbord";
             this.Text = "AdminDashbord";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSetApr;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblDashbord;
     }
 }
