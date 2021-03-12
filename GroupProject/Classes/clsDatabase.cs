31939e8d7b7f2cf8adecebb7c5256f6acc0c9eb0
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-12 21:31:56 +0000

    Update Databse string. Get user car config

--- GroupProject/Classes/clsDatabase.cs
+++ GroupProject/Classes/clsDatabase.cs
@@ -1,23 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql;
 using MySql.Data.MySqlClient;
 
-namespace practical_16_11.Classes
+namespace GroupProject.Classes
 {
 
   class clsDatabase
   {
-    private const string connString = "Server=plesk.remote.ac;database=WS295203_COllabDB;user=WS295203_CollabUser;password=CollabUser1;CharSet=utf8;";
+    private const string connString = "Server=plesk.remote.ac;database=ws330584_dealership;user=ws330584_dealership;password=Password123;CharSet=utf8;";
 
     public MySqlConnection GetConnection()
     {
       MySqlConnection cnn = new MySqlConnection(connString);
 
-      return cnn; 
+      return cnn;
+    }
+
+    // Temporary hard coded value. We will get the actual ad when user selects a car
+    public void GetUserCarConfigurations(int ID = 1)
+    {
+      var conn = this.GetConnection();
+      // Get all configurations for that user
+      // Get all the details for each configuration
+      MySqlCommand command = new MySqlCommand("GetUserCarConfigurations", conn);
+      command.CommandType = CommandType.StoredProcedure;
+
+      conn.Open();
+
+      command.Parameters.AddWithValue("@ID", ID);
+      command.Prepare();
+      MySqlDataReader reader = command.ExecuteReader();
+
+      while (reader.Read())
+      {
+        Console.WriteLine(reader.GetValue(0));
+      }
+
+      conn.Close();
     }
   }
 }
