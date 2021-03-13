e27b3ba303ae28ee630cc65bce6f227f91286e40
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-13 00:50:35 +0000

    Get user config data and insert into List<T>

--- GroupProject/Classes/clsDatabase.cs
+++ GroupProject/Classes/clsDatabase.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GroupProject.Classes.User;
 using MySql;
 using MySql.Data.MySqlClient;
 
@@ -12,7 +13,7 @@ namespace GroupProject.Classes
 
   class clsDatabase
   {
-    private const string connString = "Server=plesk.remote.ac;database=ws330584_dealership;user=ws330584_dealership;password=Password123;CharSet=utf8;";
+    private const string connString = "Server=plesk.remote.ac;database=WS295203_new_db;user=WS295203_CollabUser;password=okcpli4t94;CharSet=utf8;";
 
     public MySqlConnection GetConnection()
     {
@@ -32,16 +33,53 @@ namespace GroupProject.Classes
 
       conn.Open();
 
+
       command.Parameters.AddWithValue("@ID", ID);
       command.Prepare();
-      MySqlDataReader reader = command.ExecuteReader();
 
-      while (reader.Read())
+      // Alternative approach
+      //using (MySqlDataAdapter adapter = new MySqlDataAdapter())
+      //{
+      //  adapter.SelectCommand = command;
+      //  using (DataSet ds = new DataSet())
+      //  {
+      //    adapter.Fill(ds);
+      //  }
+      //}
+
+      List<clsUserConfiguration> userConfigurations = new List<clsUserConfiguration>();
+      List<clsUserCarConfiguration> userCarConfigurations = new List<clsUserCarConfiguration>();
+
+      using (MySqlDataReader reader = command.ExecuteReader())
       {
-        Console.WriteLine(reader.GetValue(0));
-      }
 
+        while (reader.Read())
+        {
+          userConfigurations.Add(new clsUserConfiguration { 
+            ID = reader.GetInt32(0),
+            description = reader.GetString(1),
+            user_id = reader.GetInt32(2),
+            car_id = reader.GetInt32(3)
+          });
+        }
+
+        if (reader.NextResult())
+        {
+          while (reader.Read())
+          {
+            userCarConfigurations.Add(new clsUserCarConfiguration
+            {
+              ID = reader.GetInt32(0),
+              user_id = reader.GetInt32(1),
+              modification = reader.GetInt32(2),
+              configuration_id = reader.GetInt32(3)
+            });
+          }
+        }
+      }
       conn.Close();
     }
+
+
   }
 }
