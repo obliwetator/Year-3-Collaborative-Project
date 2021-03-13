52d193a44c3980d5a4f5b71182762312c65a0766
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-13 00:40:07 +0000

    Added classes for user car configuration

---
+++ GroupProject/Classes/User/clsUserCarConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.Classes.User
+{
+  class clsUserCarConfiguration
+  {
+    public int ID { get; set; }
+    public int user_id { get; set; }
+    public int modification { get; set; }
+    public int configuration_id { get; set; }
+  }
+}
---
+++ GroupProject/Classes/User/clsUserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.Classes.User
+{
+  class clsUserConfiguration
+  {
+    public int ID { get; set; }
+    public string description { get; set; }
+    public int user_id { get; set; }
+    public int car_id { get; set; }
+  }
+}
