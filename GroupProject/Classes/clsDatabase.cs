4aecc9f324ec59396fb3d9687cbbe6912121d4c0
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-15 01:49:20 +0000

    Add get car fuction for temporay testing

--- GroupProject/Classes/clsDatabase.cs
+++ GroupProject/Classes/clsDatabase.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GroupProject.Classes.Car;
 using GroupProject.Classes.User;
 using MySql;
 using MySql.Data.MySqlClient;
@@ -13,11 +14,11 @@ namespace GroupProject.Classes
 
   class clsDatabase
   {
-    private const string connString = "Server=plesk.remote.ac;database=WS295203_new_db;user=WS295203_CollabUser;password=okcpli4t94;CharSet=utf8;";
+    private const string ConnString = "Server=plesk.remote.ac;database=ws330584_dealership;user=ws330584_dealership;password=Password123;CharSet=utf8;";
 
     public MySqlConnection GetConnection()
     {
-      MySqlConnection cnn = new MySqlConnection(connString);
+      MySqlConnection cnn = new MySqlConnection(ConnString);
 
       return cnn;
     }
@@ -28,12 +29,12 @@ namespace GroupProject.Classes
       var conn = this.GetConnection();
       // Get all configurations for that user
       // Get all the details for each configuration
-      MySqlCommand command = new MySqlCommand("GetUserCarConfigurations", conn);
-      command.CommandType = CommandType.StoredProcedure;
+      MySqlCommand command = new MySqlCommand("GetUserCarConfigurations1", conn)
+      {
+        CommandType = CommandType.StoredProcedure
+      };
 
       conn.Open();
-
-
       command.Parameters.AddWithValue("@ID", ID);
       command.Prepare();
 
@@ -55,31 +56,68 @@ namespace GroupProject.Classes
 
         while (reader.Read())
         {
-          userConfigurations.Add(new clsUserConfiguration { 
+          userConfigurations.Add(new clsUserConfiguration
+          {
             ID = reader.GetInt32(0),
             description = reader.GetString(1),
             user_id = reader.GetInt32(2),
             car_id = reader.GetInt32(3)
           });
         }
+		// Get next data set
+        reader.NextResult();
 
-        if (reader.NextResult())
+        while (reader.Read())
         {
-          while (reader.Read())
+          userCarConfigurations.Add(new clsUserCarConfiguration
           {
-            userCarConfigurations.Add(new clsUserCarConfiguration
-            {
-              ID = reader.GetInt32(0),
-              user_id = reader.GetInt32(1),
-              modification = reader.GetInt32(2),
-              configuration_id = reader.GetInt32(3)
-            });
-          }
+            ID = reader.GetInt32(0),
+            user_id = reader.GetInt32(1),
+            modification = reader.GetInt32(2),
+            configuration_id = reader.GetInt32(3),
+            modification_name = reader.GetString(4)
+          });
         }
       }
       conn.Close();
     }
 
+    public ClsCar GetCar(int carId)
+    {
+      var conn = this.GetConnection();
+      // Get all configurations for that user
+      // Get all the details for each configuration
+      string sql = @"SELECT t_Cars.*, t_Car_models.model, t_Cars_type.type FROM t_Cars
+      LEFT JOIN t_Car_models ON t_Cars.model = t_Car_models.ID
+      LEFT JOIN t_Cars_type ON t_Cars.type = t_Cars_type.ID
+      WHERE t_Cars.ID = 1";
+      
+      MySqlCommand command = new MySqlCommand(sql, conn);
 
+      conn.Open();
+      command.Parameters.AddWithValue("@ID", carId);
+      command.Prepare();
+
+      var reader = command.ExecuteReader();
+      // read once
+      reader.Read();
+      var car = new ClsCar
+      {
+        Id = reader.GetInt32(0),
+        ModelId = reader.GetInt32(1),
+        TypeId = reader.GetInt32(2),
+        Year = reader.GetInt32(3),
+        Model = reader.GetString(4),
+        Type = reader.GetString(5)
+      };
+      conn.Close();
+
+      return car;
+    }
+
+    public void GetCarCustomizations(int carId)
+    {
+
+    }
   }
 }
