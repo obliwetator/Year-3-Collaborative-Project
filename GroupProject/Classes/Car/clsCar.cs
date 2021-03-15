04f5545d44d1e095e92d8df316c23c8a8c9aed10
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-15 01:47:16 +0000

    Define car object

--- GroupProject/Classes/Car/clsCar.cs
+++ GroupProject/Classes/Car/clsCar.cs
@@ -1,7 +1,12 @@
 ﻿namespace GroupProject.Classes.Car
 {
-	public class clsCar
+	public class ClsCar
 	{
-		
+		public int Id { get; set; }
+		public int ModelId { get; set; }
+		public int TypeId { get; set; }
+		public int Year { get; set; }
+		public string Model { get; set; }
+		public string Type { get; set; }
 	}
 }
