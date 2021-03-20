b905dee5f35baf517e3ba4f78070dd3ffd5df2a2
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-20 02:12:39 +0000

    ConfirmCar choice init form

---
+++ GroupProject/UserConfirmCarChoice.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace GroupProject
+{
+	public partial class UserConfirmCarChoice : Form
+	{
+		private Dictionary<string, bool> _carConfigurationChosen;
+		public UserConfirmCarChoice(Dictionary<string, bool> carConfigurationChosen)
+		{
+			this._carConfigurationChosen = carConfigurationChosen;
+			InitializeComponent();
+		}
+	}
+}
