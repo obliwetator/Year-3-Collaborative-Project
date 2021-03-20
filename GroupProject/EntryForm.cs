5479883e366921c9ed29bf78fc00bcfdd04c5b06
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-20 01:06:05 +0000

    Baisc ability to select fong using checkboxes

--- GroupProject/EntryForm.cs
+++ GroupProject/EntryForm.cs
@@ -1,24 +1,19 @@
 ﻿using GroupProject.Classes;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace GroupProject
 {
   public partial class EntryForm : Form
   {
+    
+    
     // we will receive the CarId from the previous form
-    // Temporary value
-    private readonly int _carId;
-    public EntryForm(int carId = 1)
-    {
-      InitializeComponent();
-      this._carId = carId;
-    }
-
-
     private void Form1_Load(object sender, EventArgs e)
     {
-      clsDatabase clsDb = new clsDatabase();
+      ClsDatabase clsDb = new ClsDatabase();
       // Temporary call
       var car = clsDb.GetCar(_carId);
 
@@ -26,8 +21,30 @@ namespace GroupProject
       lblModel.Text = car.Model;
       lblType.Text = car.Type;
       lblYear.Text = car.Year.ToString();
-      
+
       // clsDb.GetUserCarConfigurations();
+      var carConfigurationsAvailable = clsDb.CarConfigurationsAvailable(_carId);
+      var carConfigurationCheckBoxes = new List<CheckBox>();
+      for (int i = 0; i < carConfigurationsAvailable.Count; i++)
+      {
+        carConfigurationCheckBoxes.Add(new CheckBox()
+        {
+          Location = new Point(200, 50 + (20 * i)),
+          Text = carConfigurationsAvailable[i].Modifications,
+        });
+
+        this.Controls.Add(carConfigurationCheckBoxes[i]);
+      }
+      
+    }
+
+    // Temporary value
+    private readonly int _carId;
+
+    public EntryForm(int carId = 1)
+    {
+      InitializeComponent();
+      this._carId = carId;
     }
   }
 }
