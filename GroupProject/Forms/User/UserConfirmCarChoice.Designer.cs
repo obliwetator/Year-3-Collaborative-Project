a2755bd8eb231ddb87125436a5b3860a11dd611b
Patryk Styla <43710236+PatrykStyla@users.noreply.github.com>
2021-03-26 02:56:12 +0000

    Added more labels. Save config as JSON

--- GroupProject/Forms/User/UserConfirmCarChoice.Designer.cs
+++ GroupProject/Forms/User/UserConfirmCarChoice.Designer.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel;
 
-namespace GroupProject
+namespace GroupProject.Forms.User
 {
 	partial class UserConfirmCarChoice
 	{
@@ -31,11 +31,169 @@ namespace GroupProject
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.btnPurchase = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.lblCarModel = new System.Windows.Forms.Label();
+			this.lblCarType = new System.Windows.Forms.Label();
+			this.lblCarYear = new System.Windows.Forms.Label();
+			this.lblCarPrice = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(147, 333);
+			this.btnSave.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(166, 79);
+			this.btnSave.TabIndex = 0;
+			this.btnSave.Text = "Save Configuration";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// btnPurchase
+			// 
+			this.btnPurchase.Location = new System.Drawing.Point(580, 333);
+			this.btnPurchase.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.btnPurchase.Name = "btnPurchase";
+			this.btnPurchase.Size = new System.Drawing.Size(166, 79);
+			this.btnPurchase.TabIndex = 1;
+			this.btnPurchase.Text = "Purchase";
+			this.btnPurchase.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(120, 58);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(118, 24);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "You\'ve chosen";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(120, 100);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(76, 24);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Car Model";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(120, 124);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(76, 24);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Car Type";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(120, 148);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(76, 24);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Car Year";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(120, 172);
+			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(76, 24);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Car Price";
+			// 
+			// lblCarModel
+			// 
+			this.lblCarModel.Location = new System.Drawing.Point(204, 100);
+			this.lblCarModel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblCarModel.Name = "lblCarModel";
+			this.lblCarModel.Size = new System.Drawing.Size(76, 24);
+			this.lblCarModel.TabIndex = 7;
+			this.lblCarModel.Text = "Car Model";
+			// 
+			// lblCarType
+			// 
+			this.lblCarType.Location = new System.Drawing.Point(204, 124);
+			this.lblCarType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblCarType.Name = "lblCarType";
+			this.lblCarType.Size = new System.Drawing.Size(76, 24);
+			this.lblCarType.TabIndex = 8;
+			this.lblCarType.Text = "Car Model";
+			// 
+			// lblCarYear
+			// 
+			this.lblCarYear.Location = new System.Drawing.Point(204, 148);
+			this.lblCarYear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblCarYear.Name = "lblCarYear";
+			this.lblCarYear.Size = new System.Drawing.Size(76, 24);
+			this.lblCarYear.TabIndex = 9;
+			this.lblCarYear.Text = "Car Model";
+			// 
+			// lblCarPrice
+			// 
+			this.lblCarPrice.Location = new System.Drawing.Point(204, 172);
+			this.lblCarPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblCarPrice.Name = "lblCarPrice";
+			this.lblCarPrice.Size = new System.Drawing.Size(76, 24);
+			this.lblCarPrice.TabIndex = 10;
+			this.lblCarPrice.Text = "Car Model";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(400, 58);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(160, 24);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Modifications chosen";
+			// 
+			// UserConfirmCarChoice
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(905, 524);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.lblCarPrice);
+			this.Controls.Add(this.lblCarYear);
+			this.Controls.Add(this.lblCarType);
+			this.Controls.Add(this.lblCarModel);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnPurchase);
+			this.Controls.Add(this.btnSave);
+			this.Font = new System.Drawing.Font("JetBrains Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.Name = "UserConfirmCarChoice";
 			this.Text = "UserConfirmCarChoice";
+			this.Load += new System.EventHandler(this.UserConfirmCarChoice_Load);
+			this.ResumeLayout(false);
 		}
+		
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label lblCarModel;
+		private System.Windows.Forms.Label lblCarType;
+		private System.Windows.Forms.Label lblCarYear;
+		private System.Windows.Forms.Label lblCarPrice;
+		
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.Button btnPurchase;
 
 		#endregion
 	}
--- GroupProject/Forms/User/UserConfirmCarChoice.cs
+++ GroupProject/Forms/User/UserConfirmCarChoice.cs
@@ -1,15 +1,89 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
+using GroupProject.Classes.Car;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-namespace GroupProject
+namespace GroupProject.Forms.User
 {
 	public partial class UserConfirmCarChoice : Form
 	{
-		private Dictionary<string, bool> _carConfigurationChosen;
-		public UserConfirmCarChoice(Dictionary<string, bool> carConfigurationChosen)
+		private ClsCar _car;
+
+		public UserConfirmCarChoice(ClsCar car)
 		{
-			this._carConfigurationChosen = carConfigurationChosen;
+			this._car = car;
 			InitializeComponent();
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			// Displays a SaveFileDialog so the user can save the Configuration
+			// assigned to btnSave.
+			SaveFileDialog sfd = new SaveFileDialog
+			{
+				Filter = "JSON File|*.json",
+				Title = "Save an Image File"
+			};
+
+			// to JSON
+			string carJson = JsonSerializer.Serialize(_car);
+
+			if (sfd.ShowDialog() != DialogResult.OK) return;
+			// Don't proceed if user doesn't input file name in the prompt
+			if (sfd.FileName != "")
+			{
+				File.WriteAllText(sfd.FileName, carJson);
+			}
+		}
+
+		private void UserConfirmCarChoice_Load(object sender, EventArgs e)
+		{
+			lblCarModel.Text = _car.Model;
+			lblCarPrice.Text = _car.Price.ToString();
+			lblCarType.Text = _car.Type;
+			lblCarYear.Text = _car.Year.ToString();
+			
+			DisplayCarModificationsLabels();
+		}
+
+		private void DisplayCarModificationsLabels()
+		{
+			// Dynamically create labels
+			int i = 0;
+			foreach (var pair in _car.CarConfigurationsChosen)
+			{
+				// Configuration is selected, show it
+				if (pair.Value == true)
+				{
+					// Name label
+					this.Controls.Add(new Label()
+					{
+						Location = new Point(400, 100 + (20 * i)),
+						// TODO: Display name instead of the ID
+						Text = pair.Key,
+						AutoSize = true,
+					});
+					
+					// Price label
+					this.Controls.Add(new Label()
+					{
+						Location = new Point(440, 100 + (20 * i)),
+						// TODO: Display price instead of the ID
+						Text = pair.Key + "£",
+						AutoSize = true,
+					});
+				}
+				i++;
+			}
+		}
+
+		private void btnPurchase_Click(object sender, EventArgs e)
+		{
+			throw new System.NotImplementedException();
 		}
 	}
 }
