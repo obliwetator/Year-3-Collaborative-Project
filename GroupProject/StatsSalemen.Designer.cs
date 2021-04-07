b908a949599e6c4395085f31b238bc8a8e67a2e3
WS330584 <WS330584@weston.ac.uk>
2021-04-07 18:36:47 +0100

    Created stats screen form for salemen

---
+++ GroupProject/StatsSalemen.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace GroupProject
+{
+    partial class StatsSalemen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboSalesmen = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblComplete = new System.Windows.Forms.Label();
+            this.lblOutstanding = new System.Windows.Forms.Label();
+            this.lblIncome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pick a salesmen";
+            // 
+            // cboSalesmen
+            // 
+            this.cboSalesmen.FormattingEnabled = true;
+            this.cboSalesmen.Location = new System.Drawing.Point(47, 87);
+            this.cboSalesmen.Name = "cboSalesmen";
+            this.cboSalesmen.Size = new System.Drawing.Size(149, 28);
+            this.cboSalesmen.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Completed Invoicse";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Outstanding invoices";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(650, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Income made";
+            // 
+            // lblComplete
+            // 
+            this.lblComplete.AutoSize = true;
+            this.lblComplete.Location = new System.Drawing.Point(86, 246);
+            this.lblComplete.Name = "lblComplete";
+            this.lblComplete.Size = new System.Drawing.Size(51, 20);
+            this.lblComplete.TabIndex = 5;
+            this.lblComplete.Text = "label5";
+            // 
+            // lblOutstanding
+            // 
+            this.lblOutstanding.AutoSize = true;
+            this.lblOutstanding.Location = new System.Drawing.Point(364, 246);
+            this.lblOutstanding.Name = "lblOutstanding";
+            this.lblOutstanding.Size = new System.Drawing.Size(51, 20);
+            this.lblOutstanding.TabIndex = 6;
+            this.lblOutstanding.Text = "label6";
+            // 
+            // lblIncome
+            // 
+            this.lblIncome.AutoSize = true;
+            this.lblIncome.Location = new System.Drawing.Point(679, 246);
+            this.lblIncome.Name = "lblIncome";
+            this.lblIncome.Size = new System.Drawing.Size(51, 20);
+            this.lblIncome.TabIndex = 7;
+            this.lblIncome.Text = "label7";
+            // 
+            // StatsSalemen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblIncome);
+            this.Controls.Add(this.lblOutstanding);
+            this.Controls.Add(this.lblComplete);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboSalesmen);
+            this.Controls.Add(this.label1);
+            this.Name = "StatsSalemen";
+            this.Text = "StatsSalemen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboSalesmen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblComplete;
+        private System.Windows.Forms.Label lblOutstanding;
+        private System.Windows.Forms.Label lblIncome;
+    }
+}
